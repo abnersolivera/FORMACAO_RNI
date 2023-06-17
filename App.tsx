351f0a5a8efd105169d9865295dedb76aa4574d8
abnersolivera <abnersanto2014@gmail.com>
2023-06-17 16:10:32 -0300

    feat: Adicionado novo componente de Avatar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components/native';
+import Avatar from './src/common/components/Avatar';
 import Container from './src/common/components/Container';
 import Content from './src/common/components/Content';
 import Icon from './src/common/components/Icon';
@@ -16,6 +17,11 @@ export default function App() {
           <Icon icon="home" color='red' size={40}></Icon>
           <Spacer height={30} />
           <Text size={30} color='onSecondary'>Text2</Text>
+          <Avatar />
+          <Avatar
+            size="large"
+            url="https://br.web.img3.acsta.net/pictures/18/08/01/20/13/3535250.jpg"
+          />
         </Content>
       </Container>
     </ThemeProvider>
